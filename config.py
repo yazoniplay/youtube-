2ff0e0ead9b2ef0c14a3ae4b4ee155fba6a98d87
yazoniplay <yazan.mahmoud2@outlook.com>
2026-09-24 10:33:47 +0200

    Add environment configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
